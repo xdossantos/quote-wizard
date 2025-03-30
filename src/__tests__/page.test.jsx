06a991905e46f30522cdcc6a5b4800d0be796071
Xola dos Santos <xola.dos.santos@accenture.com>
2025-03-30 17:14:06 +0200

    🧪 test: set up jest tests

---
+++ src/__tests__/page.test.jsx
@@ -0,0 +1,13 @@
+import '@testing-library/jest-dom'
+import { render, screen } from '@testing-library/react'
+import Page from '../app/page'
+ 
+describe('Page', () => {
+  it('renders a main', () => {
+    render(<Page />)
+ 
+    const main = screen.getByRole('main', {})
+ 
+    expect(main).toBeInTheDocument()
+  })
+})
